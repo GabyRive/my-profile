fb6ef27edb17922b4201a78dbbe8d49b6cb3f1b6
GabyRive <gabrielarivera@GabrielsMacBook.lan>
2017-01-10 13:15:55 -0400

    This is the first part of my profile

---
+++ public/assets/js/app.js
@@ -0,0 +1,11 @@
+var feed = new Instafeed({
+  get: 'user',
+  tagName: 'gabyrive',
+  userId: '8754768',
+  clientId: '306ac5a4092a418a870034646197b48b',
+  target: 'instafeed',
+  accessToken: '8754768.306ac5a.f7002047686f405791750fb3aa847f4a',
+  limit: 12
+
+});
+feed.run();
